a1ce53efe1ce23caf82f4e8679f0a768c7ef68dd
HilaEyal23 <hilaeyal04@gmail.com>
2022-10-13 20:24:18 +0300

    multArgsSupport

--- shell.c
+++ shell.c
@@ -5,6 +5,7 @@ int main(int argc, char *argv[], char *evnp[])
   char *argv_child[] = {NULL, NULL};
   char *evnp_child[] = {NULL};
   char directory[MAX_CMD_LENGTH] = {0};
+  int argc_child = {0};
   int status = {0};
 
   while(true)
@@ -12,7 +13,9 @@ int main(int argc, char *argv[], char *evnp[])
       initCmd(&directory);
       type_prompt();
       getCmd(&directory, argv_child, evnp_child);
+      argc_child = getArgv(argv_child);
       /*printCurrState(&directory, argv_child, evnp_child);*/
+      printArgs(argc_child, argv_child);
       int cpid = fork();
       if(cpid == FORK_ERROR)
       {
@@ -20,10 +23,13 @@ int main(int argc, char *argv[], char *evnp[])
         exit(1);
       }
       if(cpid)
+      {
+        printf("waiting\n");
         waitpid(-1, &status, 0);
+      }
       else
         {
-        if (execve((const char*)directory, argv_child, evnp_child) == EXECVE_FAILED)
+         if (execve((const char*)directory, argv_child, evnp_child) == EXECVE_FAILED)
               printf("%s: command not found\n", argv_child[0]);
         }
       free(argv_child[0]);
@@ -64,9 +70,72 @@ void getCmd(char directory[], char *argv_child[], char *evnp_child[])
   strcat(directory, cmd);
 }
 
+int getArgv(char *argv_child[])
+{
+  int argc_child = {1};
+  char *line = NULL;
+  size_t len = 0;
+  ssize_t lineSize = 0;
 
+  /*perror("befor if");*/
+  if(getline(&line, &len, stdin))
+  {
+    if(isEmpty(line))
+      return 0;
+   /* perror("inside if");
+    printf("line: <%s>\n", line);*/
+    char *token = NULL;
+    /*perror("before strtok");*/
+    token = strtok(line, " ");
+    /*perror("after strtok");
+    printf("first token: <%s>\n", token);*/
+    while(token)
+      {
+        if(isEmpty(token)) /* to be checked if there is a better option */
+          break;
+        argv_child[argc_child] = (char*)malloc(strlen(token));
+        strcpy(argv_child[argc_child], token);
+        /*printf("%s\n", token);*/
+        ++argc_child;
+        token = strtok(NULL, " ");
+      }
+    /*scanf("%s", arg);
+    argv_child[i] = (char*)malloc(strlen(arg));
+    strcpy(argv_child[i], arg);*/
+  }
+  /*perror("after if");*/
+  free(line);
+  return argc_child;
+}
+
+/*DEBUGGING TOOLS*/
 
 void printCurrState(char directory[], char *argv_child[], char *evnp_child[])
 {
   printf("\e[1;31mcurr state:\e[0m \ndir: %s \nargv: %s \nevnp: %s\n", directory, argv_child[0], evnp_child[0]);
+}
+
+void printArgs(int argc, char *argv[])
+{
+  printf("\e[1;31marguments:\e[0m\n");
+  int i = {0};
+  for(i; i < argc; ++i)
+    {
+      printf("The %d argument is: %s\n", i, argv[i]);
+    }
+}
+
+/*UTILITIES*/
+
+int isEmpty(char *line)
+{
+  char currChar;
+  currChar = *line;
+  while(currChar != '\n')
+    {
+      if(!isspace(currChar))
+        return false;
+      ++currChar;
+    }
+  return true;
 }
--- shell.h
+++ shell.h
@@ -6,8 +6,10 @@
 #include <string.h> /* for strcpy() */
 #include <unistd.h>
 #include <sys/wait.h>
+#include <ctype.h> /* for isspace() */ 
 
 #define true 1
+#define false 0
 #define FORK_ERROR -1
 #define EXECVE_FAILED -1
 #define PBUFF_SIZE 128
@@ -17,6 +19,9 @@
 void initCmd(char directory[]);
 void type_prompt();
 void getCmd(char directory[], char *argv_child[], char *evnp_child[]);
+int getArgv(char *argv_child[]);
 void printCurrState(char directory[], char *argv_child[], char *evnp_child[]);
+void printArgs(int argc, char *argv[]);
+int isEmpty(char *line);
 
 #endif
